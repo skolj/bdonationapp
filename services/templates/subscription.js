5da027c0cbca456a9b3591f8a995efba30262191
Sai Chand <saichand.kolloju@gmail.com>
2022-04-20 12:10:06 -0400

    Deployment commit

---
+++ services/templates/subscription.js
@@ -0,0 +1,19 @@
+module.exports = () => {
+  return `
+          <html>
+  
+              <body>
+  
+                  <div style="text-align: center">
+                      <h3>Monthly Blood Availability Reminder!</h3>
+                      <p>It's the beginning of a new month.</p>
+                      <p>Please search for donors <a href="https://bdonationapp.herokuapp.com">here</a> before it's too late.</p>
+                      
+                  </div>
+  
+              </body>
+  
+          </html>
+      
+      `;
+};
